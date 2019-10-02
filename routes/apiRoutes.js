fa07c87c4e3505e603bf93fe3c327a19a071741e
KSK <kurtknosala@gmail.com>
2019-10-01 19:33:27 -0600

    api get routes done

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,34 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
+  // Get all products
   app.get("/api/products", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+    db.Product.findAll({}).then(function(results) {
+      res.json(results);
     });
   });
-
+  // Get Specific Product
+  // By Name
+  app.get('/api/products/name/:name', function(req, res) {
+    db.Product.findOne({where: {name: req.params.name} }).then(function(results) {
+      res.json(results);
+    });
+  });
+  // Get By Mood
+  app.get('/api/products/mood/:mood', function(req, res) {
+    db.Product.findAll({where: {mood: req.params.mood} }).then(function(results) {
+      res.json(results);
+    });
+  });
+  // Get By Race
+  app.get('/api/products/race/:race', function(req, res) {
+    db.Product.findAll({where: {race: req.params.race}}).then(function(results) {
+      res.json(results);
+    });
+  });
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  app.post("/api/products", function(req, res) {
+    db.Product.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
