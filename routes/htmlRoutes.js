8cc56660aa587c0c90c2651dfe1247c276413274
KSK <kurtknosala@gmail.com>
2019-10-03 12:34:34 -0600

    admin all displaying

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,14 @@
 var db = require("../models");
+var Sequelize = require("sequelize");
 
 module.exports = function(app) {
+  const Op = Sequelize.Op;
   // Load index page
   app.get("/", function(req, res) {
     res.render("index");
   });
   // Loads search page
-  app.get("/search", function(req, res) {
+  app.get("/product", function(req, res) {
     res.render("search");
   });
   // Example Product Route. Will expand once tables have been created with data.
@@ -19,7 +21,12 @@ module.exports = function(app) {
   });
   // Loads admin page
   app.get("/admin", function(req, res) {
-    res.render("admin");
+    var all;
+    var low;
+
+    db.Product.findAll({}).then(function(results) {
+      res.render("admin", { strain: results });
+    })
   });
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
