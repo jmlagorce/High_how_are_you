2aa602aada834bec6355abdcff0d43b8d8ca0b20
KSK <kurtknosala@gmail.com>
2019-10-03 13:00:52 -0600

    admin page calls done

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -63,9 +63,21 @@ module.exports = function(app) {
   });
   // Loads admin page
   app.get("/admin", function(req, res) {
-    db.Product.findAll({}).then(function(results) {
-      res.render("admin", { strain: results });
-    });
+    const Op = Sequelize.Op;
+    const all_inventory = db.Product.findAll({});
+    const low_inventory = db.Product.findAll({
+      where: {
+        stock: {
+          [Op.lte]: 5
+        }
+      }
+    })
+    Promise.all([all_inventory, low_inventory]).then(responses => {
+      res.render("admin", { strain: responses[0], lowstrain: responses[1]});
+    })
+    // db.Product.findAll({}).then(function(results) {
+    //   res.render("admin", { strain: results });
+    // });
   });
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
