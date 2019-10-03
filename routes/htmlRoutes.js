e385acdefce974dabbeb6942a8701cd426aa3063
Nina Shin <48607661+ninashin66@users.noreply.github.com>
2019-10-02 21:03:40 -0600

    cleaned up ui on main page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,7 +8,7 @@ module.exports = function(app) {
   // Loads search page
   app.get("/search", function(req, res) {
     res.render("search");
-  })
+  });
   // Example Product Route. Will expand once tables have been created with data.
   app.get("/product/:id", function(req, res) {
     res.render("product");
@@ -16,11 +16,11 @@ module.exports = function(app) {
   // Loads checkout page
   app.get("/checkout", function(req, res) {
     res.render("checkout");
-  })
+  });
   // Loads admin page
   app.get("/admin", function(req, res) {
     res.render("admin");
-  })
+  });
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
