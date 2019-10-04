3a94e1a75181a0aa2cfcc45f54be39cf7fdff768
Nina Shin <48607661+ninashin66@users.noreply.github.com>
2019-10-03 21:15:08 -0600

    session storage admin page

--- public/js/index.js
+++ public/js/index.js
@@ -143,19 +143,11 @@ $(".update-submit").on("click", function(event) {
   }
 });
 
-<<<<<<< HEAD
+// checkout page
 $(".checkout-btn").on("click", function() {
   $(".checkout").hide();
+  alert("Thank You Come Again");
+  $(".thanks").show();
+  $(location).attr("href", "/");
 });
-=======
-
-// checkout page
-$(".checkout-btn").on ("click", function() {
-  $(".checkout").hide()
-  alert('Thank You Come Again');
-  $(".thanks").show()
-  $(location).attr('href', '/')
-
-})
- master
->>>>>>> b909aa44be2de2d8121e8c7cd78c635379e0e79d
+master;
