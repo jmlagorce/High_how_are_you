5871a67c4d44909d7cd82c701cb6a3c05f201004
Joshua LaGorce <joshlagorce@gmail.com>
2019-10-05 12:58:03 -0600

    added banner to product page

--- public/js/index.js
+++ public/js/index.js
@@ -142,8 +142,7 @@ $(".update-submit").on("click", function(event) {
 });
 
 
-$('.dropdown-toggle').dropdown()
-// checkout page
+
 $(".checkout-btn").on("click", function() {
   $(".checkout").hide();
   alert("Thank You Come Again");
